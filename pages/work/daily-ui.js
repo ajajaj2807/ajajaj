d78d9ff90f2420a4aafd7557da961266e92cc609
ajay@neolen.com <ajay@neolen.com>
2020-08-03 10:57:46 +0530

    daily-ui added

--- pages/work/daily-ui.js
+++ pages/work/daily-ui.js
@@ -1,10 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+const pics = {
+  hidden: { opacity: 0, y: 20 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      damping: 0,
+      staggerChildren: 0.3,
+    },
+  },
+};
 
 export default function pid() {
   return (
     <div className="page-wrapper">
-      <div className="work-header">
+      <motion.div
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1, transition: { damping: 0, delay: 0.4 } }}
+        className="work-header"
+      >
         <Link href="/">
           <a className="back-btn">Home</a>
         </Link>
@@ -12,10 +29,24 @@ export default function pid() {
         <span className="work-des">
           Here are some shots from the Daily UI challenge
         </span>
-      </div>
-      <div className="work-content">
-          content go brrrrr
-      </div>
+      </motion.div>
+      <motion.div
+        variants={pics}
+        initial="hidden"
+        animate="show"
+        className="work-content"
+      >
+        <motion.img variants={pics} src="/Sign Up.png" />
+        <motion.img variants={pics} src="/404.png" />
+        <motion.img variants={pics} src="/Credit Card Checkout.png" />
+        <motion.img variants={pics} src="/eCommerce- Single Page.png" />
+        <motion.img variants={pics} src="/Landing Page.png" />
+        <motion.img variants={pics} src="/User Profile.png" />
+        <motion.div variants={pics} className="container">
+          <img src="/Music App.png" />
+          <img src="/Social Share.png" />
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
